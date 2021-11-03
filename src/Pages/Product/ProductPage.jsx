46966e4e042162722901e6d508617f5c1ac2e664
Spencer D <spencer.dedrick@gmail.com>
2021-11-03 01:13:19 -0500

    made product page more responsive. still a work in progress.

--- src/Pages/Product/ProductPage.jsx
+++ src/Pages/Product/ProductPage.jsx
@@ -20,14 +20,28 @@ const StyledProductPage = styled.div`
   .product-page__full-specs-container {
     background: white;
     border: none;
-    margin: 2rem 0;
+  }
+
+  .main-image {
+    max-width: 300px;
+  }
+  .second-image {
+    max-width: 50%;
+  }
+
+  p {
+    max-width: 100ch;
+  }
+
+  .desktop-detail-text {
+    max-width: 45ch;
   }
 `;
 
 function ProductPageBreadCrumbs(props) {
   let { product } = props;
   return (
-    <div className="py-3">
+    <div className="p-3 md:bg-white">
       <Link className="pr-2 text-gray-500 text-xs" to="/">
         Home
       </Link>
@@ -46,45 +60,61 @@ function ProductPageBreadCrumbs(props) {
 function ProductPageFeatures(props) {
   let { product } = props;
   return (
-    <div>
-      <p className="text-xl font-bold tracking-widest border-b-2 my-5 border-black w-full ">
-        Product Features
-      </p>
-      <div className="border-b-2 my-3">
-        <p className="font-medium text-gray-500 my-2 tracking-wide">Design</p>
-        <p className="text-sm tracking-wide leading-relaxed  py-3">
-          {product.design}
-        </p>
-      </div>
-      <div className="border-b-2 my-3">
-        <p className="font-medium text-gray-500 my-2 tracking-wide">
-          Durability
-        </p>
-        <p className="text-sm tracking-wide leading-relaxed  py-3">
-          {product.durability}
-        </p>
-      </div>
-      <div className="border-b-2 my-3">
-        <p className="font-medium text-gray-500 my-2 tracking-wide">
-          Special Features
-        </p>
-        <p className="text-sm tracking-wide leading-relaxed  py-3">
-          {product.specialFeature}
-        </p>
-      </div>
-      <div className="border-b-2 my-3">
-        <p className="font-medium text-gray-500 my-2 tracking-wide">Movement</p>
-        <p className="text-sm tracking-wide leading-relaxed  py-3">
-          {product.movement}
-        </p>
-      </div>
-      <div className="border-b-2 my-3">
-        <p className="font-medium text-gray-500 my-2 tracking-wide">
-          Water Rating
-        </p>
-        <p className="text-sm tracking-wide leading-relaxed  py-3">
-          {product.waterRating}
-        </p>
+    <div className="bg-white">
+      <hr className="py-5 md:hidden" />
+      <div className="md:bg-white md:w-full">
+        <div className="md:flex   m-auto md:justify-between md:max-w-6xl md:mb-10">
+          <div className="md:mr-5">
+            <p className="text-xl font-bold tracking-widest border-b-2 my-5 border-black w-full">
+              Product Features
+            </p>
+            <div className="border-b-2 my-3">
+              <p className="font-medium text-gray-500 my-2 tracking-wide">
+                Design
+              </p>
+              <p className="text-sm tracking-wide leading-relaxed  py-3">
+                {product.design}
+              </p>
+            </div>
+            <div className="border-b-2 my-3">
+              <p className="font-medium text-gray-500 my-2 tracking-wide">
+                Durability
+              </p>
+              <p className="text-sm tracking-wide leading-relaxed  py-3">
+                {product.durability}
+              </p>
+            </div>
+            <div className="border-b-2 my-3">
+              <p className="font-medium text-gray-500 my-2 tracking-wide">
+                Special Features
+              </p>
+              <p className="text-sm tracking-wide leading-relaxed  py-3">
+                {product.specialFeature}
+              </p>
+            </div>
+            <div className="border-b-2 my-3">
+              <p className="font-medium text-gray-500 my-2 tracking-wide">
+                Movement
+              </p>
+              <p className="text-sm tracking-wide leading-relaxed  py-3">
+                {product.movement}
+              </p>
+            </div>
+            <div className="border-b-2 my-3">
+              <p className="font-medium text-gray-500 my-2 tracking-wide">
+                Water Rating
+              </p>
+              <p className="text-sm tracking-wide leading-relaxed  py-3">
+                {product.waterRating}
+              </p>
+            </div>
+          </div>
+          <img
+            className="w-screen md:h-full second-image"
+            src={product.photos[3]}
+            alt={product.name}
+          />
+        </div>
       </div>
     </div>
   );
@@ -93,72 +123,72 @@ function ProductPageFeatures(props) {
 function ProductPageCollapse(props) {
   let { product } = props;
   return (
-    <Collapse
-      className="product-page__full-specs-container"
-      expandIcon={<FontAwesomeIcon icon={faPlus} />}
-    >
-      <p className="text-2xl font-bold">Full Specifications</p>
-      <Panel>
-        <div className="border-b-2 my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Movement
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.movement}
-          </p>
-        </div>
-        <div className="border-b-2 my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Watch Band
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.watchBand}
-          </p>
-        </div>
-        <div className="border-b-2 my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Watch Dial
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.watchDial}
-          </p>
-        </div>
-        <div className="border-b-2 my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Watch Case
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.watchCase}
-          </p>
-        </div>
-        <div className="border-b-2 my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Water Rating
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.waterRating}
-          </p>
-        </div>
-        <div className=" my-3">
-          <p className="font-medium text-gray-500 my-2 tracking-wide">
-            Warranty
-          </p>
-          <p className="text-sm tracking-wide leading-relaxed  py-3">
-            {product.warranty}
-          </p>
-        </div>
-      </Panel>
-      <hr />
-    </Collapse>
+    <div className="md:bg-white md:w-screen py-20">
+      <Collapse
+        className="product-page__full-specs-container md:max-w-6xl md:m-auto"
+        expandIcon={<FontAwesomeIcon icon={faPlus} />}
+      >
+        <p className="text-2xl font-bold">Full Specifications</p>
+        <Panel>
+          <div className="border-b-2 my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Movement
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.movement}
+            </p>
+          </div>
+          <div className="border-b-2 my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Watch Band
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.watchBand}
+            </p>
+          </div>
+          <div className="border-b-2 my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Watch Dial
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.watchDial}
+            </p>
+          </div>
+          <div className="border-b-2 my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Watch Case
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.watchCase}
+            </p>
+          </div>
+          <div className="border-b-2 my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Water Rating
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.waterRating}
+            </p>
+          </div>
+          <div className=" my-3">
+            <p className="font-medium text-gray-500 my-2 tracking-wide">
+              Warranty
+            </p>
+            <p className="text-sm tracking-wide leading-relaxed  py-3">
+              {product.warranty}
+            </p>
+          </div>
+        </Panel>
+        <hr />
+      </Collapse>
+    </div>
   );
 }
 
-function ProductPage(props) {
+function ProductPageDetailsMobile(props) {
   let { product } = props;
-
   return (
-    <StyledProductPage className="px-5 w-screen">
-      <ProductPageBreadCrumbs product={product} />
+    <div className="product-page__details-container md:hidden">
       <div className="flex w-1/2 justify-between align-middle py-2">
         <p className="font-medium text-sm">{product.caseMeasurements}</p>
         <p className="text-xs font-medium rounded-full border-black border-2 m-auto px-3">
@@ -196,10 +226,76 @@ function ProductPage(props) {
         {product.description}
       </p>
       <img className="w-screen" src={product.photos[3]} alt={product.name} />
+    </div>
+  );
+}
+function ProductPageDetailsDesktop(props) {
+  let { product } = props;
+  return (
+    <div className="bg-white mb-24 pb-20">
+      <div className="product-page__details-container sm:hidden md:flex md: justify-space-between md:max-w-6xl pt-10 md:m-auto">
+        <div>
+          <div className="flex w-1/2 justify-between align-middle py-2">
+            <p className="font-medium text-sm">{product.caseMeasurements}</p>
+            <p className="text-xs font-medium rounded-full border-black border-2 m-auto px-3">
+              {product.flair}
+            </p>
+          </div>
+          <p className="font-semibold py-2 text-xl">
+            {product.name} {product.type}
+          </p>
+          <p className="py-1 text-sm text-gray-500 font-medium">
+            {product.color}
+          </p>
+          <StarRatingComponent editing={false} />
+          <p className="desktop-detail-text text-sm tracking-wide leading-relaxed py-3">
+            {product.description}
+          </p>
+        </div>
+
+        <img
+          className=" px-3 w-11/12 mb-10 main-image"
+          src={product.image}
+          alt={product.name}
+        />
+        <div className="flex flex-col start ">
+          <p className="font-medium text-xl text-center">
+            ${product.price}.00{" "}
+          </p>
+          <p className="font-medium text-xs py-2 ">
+            <span className="font-medium text-gray-500">
+              or 4 installments of $ {product.price / 4} with{" "}
+            </span>
+            {/* make info icon a popup with modal */}
+            <span className="font-bold">
+              alterpay <FontAwesomeIcon icon={faInfoCircle} />
+            </span>
+          </p>
+          <p className="m-auto text-center my-5 text-sm font-semibold">
+            <FontAwesomeIcon icon={faTruck} /> Free Shipping + Returns
+          </p>
+          <div className="text-center my-5">
+            <Button type="black">Add to Cart</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProductPage(props) {
+  let { product } = props;
+
+  return (
+    <StyledProductPage className=" w-screen bg-gray-100">
+      <ProductPageBreadCrumbs product={product} />
+      <ProductPageDetailsMobile product={product} />
+      <ProductPageDetailsDesktop product={product} />
       <ProductPageFeatures product={product} />
       <ProductPageCollapse product={product} />
-      <HomeProductSlider />
-      <StarRatingComponent editing={false} />
+      <div className="bg-white my-20">
+        <HomeProductSlider />
+      </div>
     </StyledProductPage>
   );
 }
